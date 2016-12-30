b2118b9fd03877d938663563e3de0b82effa15f5
ZhangNX <504947517@qq.com>
2016-12-30 14:25:31 +0800

    DAO

---
+++ src/cn/bjfu/im/BaseDAO.java
@@ -0,0 +1,32 @@
+package cn.bjfu.im;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseDAO {
+	public Connection open() {
+		Connection conn = null;
+		try {
+			// 1. 加载驱动
+			Class.forName("com.mysql.jdbc.Driver");
+			// 2. 获取到指定数据库的连接对象 
+			String url = "jdbc:mysql://localhost/";////数据库名称
+			conn = DriverManager.getConnection(url, "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	public void close(Connection conn) {
+		if (conn == null)
+			return;
+		try {
+			conn.close();// 关闭连接
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
